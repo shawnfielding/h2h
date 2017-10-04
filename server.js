5bc51d112431b65278c24eb2fe275fe4a21264a6
Shawn Fielding <shawnfielding@users.noreply.github.com>
2017-10-04 11:27:24 -0400

    no clue why the css is not being pulled into the doc

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var moment = require("moment");
+var Sequelize = require("sequelize")
+
+var PORT = 3000;
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+// Import routes and give the server access to them.
+console.log(require("./app/routing/apiroutes")(app));
+console.log(require("./app/routing/htmlroutes")(app));
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
